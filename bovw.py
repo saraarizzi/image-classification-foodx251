938f46448cf366fce61e2d71b4c7e0e9520c67ac
Sara Arizzi <s.arizzi1@campus.unimib.it>
2024-08-16 07:49:33 +0200

    bovw with 5, 15, and 30 dims

--- bovw.py
+++ bovw.py
@@ -1,7 +1,6 @@
 import os
 import time
 import multiprocessing
-import joblib
 
 import cv2 as cv
 import numpy as np
@@ -19,9 +18,9 @@ def get_desc(names, d_path: str):
         image = cv.imread(os.path.join(d_path, im))
         bw_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         _, im_desc = sift.detectAndCompute(bw_image, None)
+
         if im_desc is not None:
-            pass
-            # class_desc.append(np.array(im_desc))
+            class_desc.append(np.array(im_desc))
         else:
             to_remove.append(im)
 
@@ -61,26 +60,19 @@ def get_tf_vectors(descriptors_vw, k):
     return frequency_vectors
 
 
-def get_tfidf_vectors(tf, c_code, b_path):
-    tfidf_path = os.path.join(b_path, f"{c_code}_tfidf.pkl")
+def get_tfidf_vectors(tf):
 
-    if os.path.isfile(tfidf_path):
-        tfidf = joblib.load(tfidf_path)
-    else:
-        n = len(tf)
-        df = np.sum(np.array(tf) > 0, axis=0)
-        idf = np.log(n / df)
-        tfidf = tf * idf
-        joblib.dump(tfidf, tfidf_path, compress=3)
+    n = len(tf)
+    df = np.sum(np.array(tf) > 0, axis=0)
+    idf = np.log(n / df)
+    tfidf = tf * idf
 
     return tfidf
 
 
-def process_class(params):
-    class_code = params[0]
-    k = params[1]
+def process_class(class_code):
 
-    bovw_path = os.path.join(*["data", "bovw", f"{k}_vw"])
+    bovw_path = os.path.join(*["data", "bovw", f"{class_code}"])
     os.makedirs(bovw_path, exist_ok=True)
 
     data_path = os.path.join(*["data", "clean", "train", f"{class_code}"])
@@ -91,37 +83,43 @@ def process_class(params):
     for im in images_to_remove:
         images_names.remove(im)
 
-    # Create visual words
-    codebook = get_codebook(images_desc, k)
+    for k in [5, 15, 30]:
+        # Create visual words
+        codebook = get_codebook(images_desc, k)
 
-    # Visual words vector
-    images_vw = get_vw_vectors(images_desc, codebook)
+        # Visual words vector
+        images_vw = get_vw_vectors(images_desc, codebook)
 
-    # Create tf vectors
-    images_tf = get_tf_vectors(images_vw, k)
+        # Create tf vectors
+        images_tf = get_tf_vectors(images_vw, k)
 
-    # Created tf-idf vectors
-    get_tfidf_vectors(images_tf, class_code, bovw_path)
+        # Created tf-idf vectors
+        images_tfidf = get_tfidf_vectors(images_tf)
 
+        pd.DataFrame({
+            "image_name": images_names,
+            "num_desc": list(map(lambda x: len(x), images_desc)),
+            "tfidf": list(map(lambda x: np.array2string(x, separator=",", formatter={'float_kind': lambda num: "%.6f" % num}), images_tfidf))
+        }).to_csv(os.path.join(bovw_path, f"tfidf_{k}.csv"), sep=";", index=False)
+
+    print(f"Done --- {class_code + 1}/251")
     return
 
 
 if __name__ == "__main__":
 
-    for k_vw in [5, 10, 20, 30]:
-        start_time = time.time()
-        print(f"Started {k_vw} at: {time.strftime(DATE_FORMAT)}")
+    start_time = time.time()
+    print(f"Started at: {time.strftime(DATE_FORMAT)}")
 
-        classes_data = pd.read_csv("data/class_list.txt", sep=" ", header=None)
-        classes_data.columns = ["code", "label"]
-        classes_codes = classes_data.code.tolist()
-        args = [(cl_code, k_vw) for cl_code in classes_codes]
+    classes_data = pd.read_csv("data/class_list.txt", sep=" ", header=None)
+    classes_data.columns = ["code", "label"]
+    classes_codes = classes_data.code.tolist()
 
-        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-            pool.map(process_class, args)
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        pool.map(process_class, classes_codes)
 
-        print(f"Ended {k_vw} at: {time.strftime(DATE_FORMAT)}")
+    print(f"Ended at: {time.strftime(DATE_FORMAT)}")
 
-        end_time = time.time()
-        duration = end_time - start_time
-        print(f"Script total duration for {k_vw}: {duration:.2f} seconds")
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Script total duration: {duration:.2f} seconds")
