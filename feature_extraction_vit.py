f1bf7ea65c4e5e787d4078cb40fc6b60e81733d0
Sara Arizzi <s.arizzi1@campus.unimib.it>
2024-08-23 22:35:33 +0200

    added fe from model trained on imagenet

--- feature_extraction_vit.py
+++ feature_extraction_vit.py
@@ -5,13 +5,13 @@ import time
 import numpy as np
 import pandas as pd
 from PIL import Image
-from transformers import AutoImageProcessor, AutoModelForImageClassification
+from transformers import AutoImageProcessor, AutoModelForImageClassification, ViTImageProcessor
 from transformers import ViTModel, ViTConfig
 import torch.nn as nn
 
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-HF_PATH = os.path.join("data", "hf_vit_food")
+HF_PATH = os.path.join("data", "hf")
 
 
 class ViTPooler(nn.Module):
@@ -29,23 +29,32 @@ class ViTPooler(nn.Module):
         return pooled_output
 
 
-def read_model():
-    # Load local files
-    pr = AutoImageProcessor.from_pretrained(HF_PATH)
-    m = AutoModelForImageClassification.from_pretrained(HF_PATH)
+def read_model(dataset):
+    if dataset == "food":
+        # Load from local folder
+        pr = AutoImageProcessor.from_pretrained(os.path.join(HF_PATH, "food"))
+        m = AutoModelForImageClassification.from_pretrained(os.path.join(HF_PATH, "food"))
 
-    # Add pooler
-    new_model = ViTModel(config=m.config)
-    new_model.embeddings = m.base_model.embeddings
-    new_model.encoder = m.base_model.encoder
-    new_model.layernorm = m.base_model.layernorm
-    new_model.pooler = ViTPooler(config=m.config)  # no weights
+        # Rewrite model as original ViT architecture
+        new_model = ViTModel(config=m.config)
+        new_model.embeddings = m.base_model.embeddings
+        new_model.encoder = m.base_model.encoder
+        new_model.layernorm = m.base_model.layernorm
+        new_model.pooler = ViTPooler(config=m.config)
 
-    return new_model, pr
+        return new_model, pr
+
+    elif dataset == "imagenet":
+        # Load from local folder
+        pr = ViTImageProcessor.from_pretrained(os.path.join(HF_PATH, "imagenet"))
+        m = ViTModel.from_pretrained(os.path.join(HF_PATH, "imagenet"))
+
+        return m, pr
+
+    raise Exception("Param must be in ['food', 'imagenet']")
 
 
 def process_class(class_code):
-    s_time = time.time()
     print(f"Start Processing ... {class_code}")
     fe_path = os.path.join(*["data", "fe", f"{class_code}"])
     os.makedirs(fe_path, exist_ok=True)
@@ -55,43 +64,74 @@ def process_class(class_code):
     image_data = pd.read_csv(image_info_path, header=None, names=["file", "class"])
     images_names = image_data[image_data["class"] == class_code]["file"].to_list()
 
-    # Load model
-    model, processor = read_model()
+    # Load models
+    model_food, processor_food = read_model("food")
+    model_imagenet, processor_imagenet = read_model("imagenet")
 
-    features_list = []
+    features_list_food = []
+    features_list_imagenet = []
     for image_name in images_names:
         image = Image.open(os.path.join(data_path, image_name))
 
         # Preprocess
-        inputs = processor(images=image, return_tensors="pt")
+        inputs_food = processor_food(images=image, return_tensors="pt")
+        inputs_imagenet = processor_imagenet(images=image, return_tensors="pt")
 
         # Inference
-        outputs = model(**inputs).pooler_output
+        outputs_food = model_food(**inputs_food).pooler_output
+        outputs_imagenet = model_imagenet(**inputs_imagenet).pooler_output
 
-        features = outputs.detach().numpy().reshape(-1)  # 768-dim
-        features_list.append(features)
+        # Save Food-101 features
+        features_food = outputs_food.detach().numpy().reshape(-1)  # 768-dim
+        features_list_food.append(features_food)
+
+        # Save ImageNet21k features
+        features_imagenet = outputs_imagenet.detach().numpy().reshape(-1)  # 768-dim
+        features_list_imagenet.append(features_imagenet)
 
     pd.DataFrame({
         "image_name": images_names,
         "vit_feature": list(
-            map(lambda x: np.array2string(x, separator=",", formatter={'float_kind': lambda num: "%.6f" % num}),
-                features_list)
+            map(
+                lambda x: np.array2string(x, separator=",", formatter={'float_kind': lambda num: "%.6f" % num}),
+                features_list_imagenet
+            )
         )
-    }).to_csv(os.path.join(fe_path, f"vit.csv"), sep=";", index=False)
+    }).to_csv(os.path.join(fe_path, f"vit_food.csv"), sep=";", index=False)
 
-    print(f"End Processing ... {class_code} ---> Duration {(time.time() - s_time):.2f} seconds")
-    return
+    pd.DataFrame({
+        "image_name": images_names,
+        "vit_feature": list(
+            map(
+                lambda x: np.array2string(x, separator=",", formatter={'float_kind': lambda num: "%.6f" % num}),
+                features_list_imagenet
+            )
+        )
+    }).to_csv(os.path.join(fe_path, f"vit_imagenet.csv"), sep=";", index=False)
+
+
+def download_model(processor, model, name, folder):
+    # Download from HF
+    hf_pre = processor.from_pretrained(name)
+    hf_mod = model.from_pretrained(name)
+
+    # Save locally
+    os.makedirs(os.path.join(HF_PATH, folder), exist_ok=True)
+    hf_pre.save_pretrained(os.path.join(HF_PATH, folder))
+    hf_mod.save_pretrained(os.path.join(HF_PATH, folder))
 
 
 if __name__ == "__main__":
-    # Download from HF
-    hf_pre = AutoImageProcessor.from_pretrained("nateraw/food")
-    hf_mod = AutoModelForImageClassification.from_pretrained("nateraw/food")
 
-    # Save locally
-    os.makedirs(HF_PATH, exist_ok=True)
-    hf_pre.save_pretrained(HF_PATH)
-    hf_mod.save_pretrained(HF_PATH)
+    # ViT trained on Food-101
+    download_model(
+        AutoImageProcessor, AutoModelForImageClassification, "nateraw/food", "food"
+    )
+
+    # ViT trained on ImageNet21k
+    download_model(
+        ViTImageProcessor, ViTModel, "google/vit-base-patch16-224-in21k", "imagenet"
+    )
 
     # Start processing
     start_time = time.time()
