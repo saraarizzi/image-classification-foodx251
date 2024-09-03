b77256b22d590e013b59fa10714cc4d95f80c6ef
Sara Arizzi <s.arizzi1@campus.unimib.it>
2024-09-03 09:03:58 +0200

    update

--- training/show_history.py
+++ training/show_history.py
@@ -2,6 +2,7 @@ import json
 import os
 import pickle
 
+import numpy as np
 from matplotlib import pyplot as plt
 
 DATA_PATH = os.path.join("..", "data")
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     # model_names = list_config.get("model_names")
 
     model_names = [
-        "06_mnv3_fine_tuned"
+        "full_04_mnv3_fine_tuned"
     ]
 
     for model_name in model_names:
@@ -45,4 +46,10 @@ if __name__ == "__main__":
         history_path = os.path.join(*[DATA_PATH, "saved_models", f"{model_name}_history.pkl"])
         history = pickle.load(open(history_path, "rb"))
         plot_history(history)
+
+        print(model_name)
+        print(f"Stopped at: {len(history.get('val_loss'))}")
+        print(np.round(np.max(history.get("val_acc1")) * 100, 2))
+        print(np.round(np.max(history.get("val_acc3")) * 100, 2))
+        print(np.round(np.max(history.get("val_acc5")) * 100, 2))
 
